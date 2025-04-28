b64c1865247d606a66ec81cb247955d209fe8b0b
Arka <arkapravasantra17@gmail.com>
2025-04-28 14:20:12 +0530

    I think at least complete the controllerjs

---
+++ Backend/Model/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: string, required: true, unique: true },
+    email: { type: string, required: true, unique: true },
+    password: { type: string, required: true },
+    profilePicture: { type: string, default: "" },
+    bio: { type: String, default: "" },
+    gender: { type: String, enum: ["male", "female"] },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    bookmarks: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
---
+++ Backend/index.js
@@ -0,0 +1,35 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+
+dotenv.config({});
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+
+const corsOptions = {
+    origin: 'http://localhost:5173', // Corrected spelling
+    credentials: true,
+};
+app.use(cors(corsOptions));
+
+// routes
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        message: "I am coming from backend",
+        success: true,
+    });
+});
+
+
+app.listen(PORT, () => {
+    connectDB()
+    console.log(`Server listening at port ${PORT}`);
+});
